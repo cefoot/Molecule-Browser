e5129343a229d1a2dcf7c840993d1164ba1607f1
Chris Papenfuß <chris.papenfuss@data-experts.de>
2021-11-20 09:56:03 +0100

    adding error msgs for molecule search dialog

--- molecula-shared/App.cs
+++ molecula-shared/App.cs
@@ -2,6 +2,7 @@
 using StereoKit;
 using System;
 using System.Collections.Generic;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace molecula_shared
@@ -17,6 +18,10 @@ namespace molecula_shared
         public float MoleculeScale = 0.1f;
 
         public float AtomDiameter = 0.05f;
+        private StringBuilder _errorMsg = new StringBuilder();
+        private DateTime _errorTime = DateTime.Now;
+        private readonly TextStyle _errorTextStyl = Text.MakeStyle(Font.Default, 2f * U.cm, new Color(1f, 0f, 0f));
+        private readonly TextStyle _normalTextStyle = Text.MakeStyle(Font.Default, 2f * U.cm, new Color(1f, 1f, 1f));
 
         public class AtomData
         {
@@ -34,6 +39,7 @@ namespace molecula_shared
 
         public void Loop()
         {
+            Init();
             // Create assets used by the app
             Pose windowPose = new Pose(.4f, 0, -.5f, Quat.LookDir(Input.Head.Forward * -1));
 
@@ -61,69 +67,105 @@ namespace molecula_shared
             })) ;
         }
 
+        private void Init()
+        {
+            //_errorTextStyl.Material.SetColor("color", new Color(1f, 0f, 0f));
+            //_normalTextStyle.Material.SetColor("color", Color.White);
+        }
+
         private void MoleculeSearchWindow(ref Pose windowPose, ref string moleculeSearchTxt)
         {
-            UI.WindowBegin("Molecule search", ref windowPose, new Vec2(22, 0) * U.cm, UIWin.Normal);
+            UI.WindowBegin("Molecule search", ref windowPose, new Vec2(25, 0) * U.cm, UIWin.Normal);
+            UI.PushTextStyle(_normalTextStyle);
             UI.Label("Name:");
             UI.SameLine();
             UI.Input("inpMolecule", ref moleculeSearchTxt);//, new Vec2(10, 2) * U.mm);
-            if (UI.Button("Search"))
+            if (UI.Button("Search") && !string.IsNullOrEmpty(moleculeSearchTxt))
             {
                 LoadMolecule(moleculeSearchTxt);
                 moleculeSearchTxt = "";
+            }
+            if ((DateTime.Now - _errorTime).TotalSeconds < 5d)
+            {
+                UI.PushTextStyle(_errorTextStyl);
+                UI.Text(_errorMsg.ToString(), TextAlign.Center);
+                UI.PopTextStyle();
+            }
+            else
+            {
+                _errorMsg.Clear();
             }
             UI.WindowEnd();
         }
 
         public async /*Task<Model>*/ void LoadMolecule(string moleculeName)
         {//https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/water/record/JSON/?record_type=3d
-            var client = new RestClient("https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/");
-            //client.Authenticator = new HttpBasicAuthenticator("username", "password");
-
-            var request = new RestRequest($"{moleculeName}/record/JSON/?record_type=3d", DataFormat.Json);
-
-            var response = await client.GetAsync<RecordData>(request);
-            System.Diagnostics.Debug.WriteLine(string.Join(";", response.PC_Compounds[0].AtomList));
-            var mdl = new Model();
-            foreach (var atom in response.PC_Compounds[0].AtomList)
+            try
             {
-                if (!_atomMaterialMap.ContainsKey(atom.ElementOrderNumber))
+
+                var client = new RestClient("https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/");
+                //client.Authenticator = new HttpBasicAuthenticator("username", "password");
+
+                var request = new RestRequest($"{moleculeName}/record/JSON/?record_type=3d", DataFormat.Json);
+
+                var response = await client.GetAsync<RecordData>(request);
+                if (response == null || response.PC_Compounds == null)
                 {
-                    var mat = Default.MaterialUI.Copy();
-                    mat.SetColor("color", atom.GetColor());
-                    _atomMaterialMap[atom.ElementOrderNumber] = mat;
+                    AddErrorMessage($"no molecule found with name '{moleculeName}'");
+                    return;
                 }
-                var atmPos = atom.Position * MoleculeScale;
-                var atmMdl = mdl.AddNode(atom.GetName(), new Pose(atmPos, Quat.Identity).ToMatrix(), Mesh.GenerateSphere(AtomDiameter), _atomMaterialMap[atom.ElementOrderNumber]);
-                foreach (var bndData in atom.Bonds)
+                System.Diagnostics.Debug.WriteLine(string.Join(";", response.PC_Compounds[0].AtomList));
+                var mdl = new Model();
+                foreach (var atom in response.PC_Compounds[0].AtomList)
                 {
-                    var bndPos = bndData.Key.Position * MoleculeScale;
-                    var bndDir = bndPos - atmPos;
-                    var mddlPnt = atmPos + bndDir / 2f;
-                    var bndDirNorm = Vec3.Cross(bndDir, new Vec3(bndDir.z, bndDir.x, bndDir.y)).Normalized * AtomDiameter * .5f;
-                    for (int i = 0; i < bndData.Value; i++)
+                    if (!_atomMaterialMap.ContainsKey(atom.ElementOrderNumber))
                     {
-                        var tmpPnt = mddlPnt;
-                        if (bndData.Value > 1)
+                        var mat = Default.MaterialUI.Copy();
+                        mat.SetColor("color", atom.GetColor());
+                        _atomMaterialMap[atom.ElementOrderNumber] = mat;
+                    }
+                    var atmPos = atom.Position * MoleculeScale;
+                    var atmMdl = mdl.AddNode(atom.GetName(), new Pose(atmPos, Quat.Identity).ToMatrix(), Mesh.GenerateSphere(AtomDiameter), _atomMaterialMap[atom.ElementOrderNumber]);
+                    foreach (var bndData in atom.Bonds)
+                    {
+                        var bndPos = bndData.Key.Position * MoleculeScale;
+                        var bndDir = bndPos - atmPos;
+                        var mddlPnt = atmPos + bndDir / 2f;
+                        var bndDirNorm = Vec3.Cross(bndDir, new Vec3(bndDir.z, bndDir.x, bndDir.y)).Normalized * AtomDiameter * .5f;
+                        for (int i = 0; i < bndData.Value; i++)
                         {
-                            tmpPnt += bndDirNorm * i;
+                            var tmpPnt = mddlPnt;
+                            if (bndData.Value > 1)
+                            {
+                                tmpPnt += bndDirNorm * i;
+                            }
+                            //Quat.FromAngles(0f, 0f, 360f / bndData.Value).
+                            mdl.AddNode(
+                                $"{atom.GetSymbol()}_{bndData.Key.GetSymbol()}",
+                                Matrix.TRS(
+                                    tmpPnt,
+                                    Quat.LookDir(bndDir),
+                                    new Vec3(.1f, .1f, 1f)
+                                    ),
+                                Mesh.GenerateSphere(Vec3.Distance(atom.Position * MoleculeScale, bndData.Key.Position * MoleculeScale)),
+                                _atomMaterialMap[BndClr]);
                         }
-                        //Quat.FromAngles(0f, 0f, 360f / bndData.Value).
-                        mdl.AddNode(
-                            $"{atom.GetSymbol()}_{bndData.Key.GetSymbol()}",
-                            Matrix.TRS(
-                                tmpPnt,
-                                Quat.LookDir(bndDir),
-                                new Vec3(.1f, .1f, 1f)
-                                ),
-                            Mesh.GenerateSphere(Vec3.Distance(atom.Position * MoleculeScale, bndData.Key.Position * MoleculeScale)),
-                            _atomMaterialMap[BndClr]);
                     }
                 }
+                var pos = Input.Head.position + Input.Head.Forward;
+                _molecules.Add(new AtomData { Model = mdl, Pose = new Pose(pos, Quat.LookAt(pos, Input.Head.position, Vec3.Up)), Name = moleculeName });
+                System.Diagnostics.Debug.WriteLine($"Molecule '{moleculeName}' loaded");
             }
-            var pos = Input.Head.position + Input.Head.Forward;
-            _molecules.Add(new AtomData { Model = mdl, Pose = new Pose(pos, Quat.LookAt(pos, Input.Head.position, Vec3.Up)), Name = moleculeName });
-            System.Diagnostics.Debug.WriteLine($"Molecule '{moleculeName}' loaded");
+            catch (Exception e)
+            {
+                AddErrorMessage(e.Message);
+            }
+        }
+
+        private void AddErrorMessage(string msg)
+        {
+            _errorTime = DateTime.Now;
+            _errorMsg.AppendLine(msg);
         }
     }
 }
